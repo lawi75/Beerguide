ce986d58a056a30e55d34e9d5fabbc382997f555
Lars Wiklund <lars@wiklund.ws>
2012-10-04 16:01:08 +0200

    Fixed problems with Facebook connect

--- src/ws/wiklund/beerguide/activities/BeerListActivity.java
+++ src/ws/wiklund/beerguide/activities/BeerListActivity.java
@@ -129,13 +129,13 @@ public class BeerListActivity extends CustomListActivity {
 			final File exportFile = new File(ViewHelper.getRoot(), "export_guide.csv");
 			alertDialog.setMessage(String.format(getString(R.string.export_message), new Object[]{exportFile.getAbsolutePath()}));
 			
-			alertDialog.setButton(getString(android.R.string.yes), new DialogInterface.OnClickListener() {
+			alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(android.R.string.yes), new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int which) {
 			       new ExportDatabaseCSVTask(BeerListActivity.this, new BeerDatabaseHelper(BeerListActivity.this), exportFile, getListAdapter().getCount()).execute();
 				} 
 			});
 			
-			alertDialog.setButton2(getString(android.R.string.no), new DialogInterface.OnClickListener() {
+			alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.no), new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int which) {
 					alertDialog.cancel();
 				} 
--- src/ws/wiklund/beerguide/activities/ModifyBeerActivity.java
+++ src/ws/wiklund/beerguide/activities/ModifyBeerActivity.java
@@ -1,6 +1,7 @@
 package ws.wiklund.beerguide.activities;
 
 import ws.wiklund.beerguide.db.BeerDatabaseHelper;
+import ws.wiklund.guides.activities.FullAdActivity;
 import ws.wiklund.guides.activities.ModifyBeverageActivity;
 import ws.wiklund.guides.db.BeverageDatabaseHelper;
 
@@ -9,6 +10,11 @@ public class ModifyBeerActivity extends ModifyBeverageActivity {
 	@Override
 	protected BeverageDatabaseHelper getDatabaseHelper() {
 		return new BeerDatabaseHelper(this);
+	}
+
+	@Override
+	protected Class<?> getIntentClass() {
+		return FullAdActivity.class;
 	}
 	
 }
